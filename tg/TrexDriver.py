cab8a2113374d3b006d1505f7fdbd8867de5dc74
Andrea Mayer <mayer.andrea.17@gmail.com>
2018-08-11 15:20:31 -0500

    improved version of TrexDriver.py

--- tg/TrexDriver.py
+++ tg/TrexDriver.py
@@ -15,42 +15,36 @@ from trex_stl_lib.api import *
 class TrexDriver():
     
     # Builds an instance of TrexDriver
-    def __init__(self, server, txPort, rxPort, pcap, kpps, duration):
+    def __init__(self, server, txPort, rxPort, pcap, rate, duration):
         self.server = server
         self.txPort = txPort
         self.rxPort = rxPort
         self.pcap = pcap
-        self.rate = kpps;
+        self.rate = rate;
         self.duration = duration
-        
-    # It transforms the rate expressed in kpps into the inter-packet gap
-    # expressed in micro-seconds.
-    def __fromKppsToIpgUsec(self, kpps):
-        return 1000.0 / kpps;
     
-    # It evaluates the number of times that a pcap file should be used to
-    # generate a sufficient number of packets to fill the entire tx
-    # window for completing the measurement test.
-    def __packetLoopCounter(self, ipgUsec, duration):
-        return long(math.ceil(2.0 * (1e6 / (ipgUsec))) * duration)
+    # It creates a stream by leveraging the 'pcap' file which has been set 
+    # during the driver creation.
+    def __buildStreamsFromPcap(self):
+            return [STLStream(packet=STLPktBuilder(pkt=self.pcap),
+                              mode=STLTXCont())]
         
+    # We create and return a dictionary used to store results of the run.
+    # The dictionary is composed by:
+    # 
+    # dictionary 
+    #     |
+    #     +--tx
+    #     |   +--port
+    #     |   +--total_packets
+    #     |
+    #     +--rx
+    #     |   +--port
+    #     |   +--total_packets
+    #     |
+    #     +--warnings
+    #
     def run(self):
-        
-        # We create a dictionary used to store results of the run.
-        # The dictionary is composed by:
-        # 
-        # dictionary 
-        #     |
-        #     +--tx
-        #     |   +--port
-        #     |   +--total_packets
-        #     |
-        #     +--rx
-        #     |   +--port
-        #     |   +--total_packets
-        #     |
-        #     +--warnings
-        #
         output = {}
         output['tx'] = {}
         output['rx'] = {}
@@ -66,24 +60,19 @@ class TrexDriver():
         try:
             profile = None
             stream = None
+            txStats = None
+            rxStats = None
             allPorts = [self.txPort, self.rxPort]
-            ipgUsec = self.__fromKppsToIpgUsec(self.rate)
-            loopCount = self.__packetLoopCounter(ipgUsec, self.duration)
             
             client.connect()
             
             # For safety reasons we reset any counter.
             client.reset(ports=allPorts)
             
-            # We need to create a profile by reading the pcap and setting
-            # the ipg_usec as well as the loop_count.
-            profile = STLProfile.load_pcap(self.pcap, ipg_usec=ipgUsec,
-                                           loop_count=loopCount)
-            
             # We retrieve the streams
             # NOTE: we have as many streams as captured packets within 
             # the .pcap file.
-            streams = profile.get_streams()
+            streams = self.__buildStreamsFromPcap()
             
             # We use only one port to multiplex altogether streams.
             client.add_streams(streams, ports=[self.txPort])
@@ -93,10 +82,8 @@ class TrexDriver():
             # start of the experiment some packets may be received on ports.
             client.clear_stats()
             
-            # NOTE: parameter 'mult' it does not seem to work properly when
-            # pcap is used. Conversely, the ipg_usec is used to define
-            # the packet rates.
-            client.start(ports=[self.txPort], duration=self.duration)
+            client.start(ports=[self.txPort], mult=self.rate,
+                         duration=self.duration)
             
             # Now we block until all packets have been send/received. To
             # for be sure operations had been completed we wait for both
@@ -111,8 +98,12 @@ class TrexDriver():
             # We wait for a bit in order to let the counters be stable
             sleep(1)
             
-            output['tx']['total_packets'] = client.get_xstats(self.txPort)['tx_total_packets']
-            output['rx']['total_packets'] = client.get_xstats(self.rxPort)['rx_total_packets']
+            # We retrieve statistics from Tx and Rx ports.
+            txStats = client.get_xstats(self.txPort)
+            rxStats = client.get_xstats(self.rxPort)
+            
+            output['tx']['total_packets'] = txStats['tx_total_packets']
+            output['rx']['total_packets'] = rxStats['rx_total_packets']
           
         except STLError as e:
             print(e)
@@ -127,7 +118,7 @@ class TrexDriver():
 # Entry point used for testing
 if __name__ == '__main__':
     
-    driver = TrexDriver('127.0.0.1', 0, 1, 'pcap/raw-pcap-files/plain-ipv6-64.pcap', 1100, 10)
+    driver = TrexDriver('127.0.0.1', 0, 1, 'pcap/raw-pcap-files/plain-ipv6-64.pcap', '5kpps', 10)
     output = driver.run()
     print(output) 
     
