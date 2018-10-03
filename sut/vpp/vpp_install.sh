fa3b541d22b8a8c0013b5e7b9601c5e90cd472e7
pierventre <pierventre@hotmail.com>
2018-10-03 09:06:16 +0200

    Various improvements

--- sut/vpp/vpp_install.sh
+++ sut/vpp/vpp_install.sh
@@ -6,7 +6,7 @@ pip install vpp-config
 
 # Install VPP  with APT (Latest Released Version)
 touch /etc/apt/sources.list.d/99fd.io.list
-echo 'deb [trusted=yes] https://nexus.fd.io/content/repositories/fd.io.ubuntu.xenial.main/ ./' > /etc/sudoers
+echo "deb [trusted=yes] https://nexus.fd.io/content/repositories/fd.io.ubuntu.xenial.main/ ./" | sudo tee -a /etc/apt/sources.list.d/99fd.io.list
 apt-get update
 
 apt-get install vpp-lib
