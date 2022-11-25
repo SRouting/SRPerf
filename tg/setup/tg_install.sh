f97cd14f5c9cb692cc268dbc1c4c8c8b260fea8a
Carmine Scarpitta <carmine.scarpitta@uniroma2.it>
2022-11-25 10:23:34 -0600

    wip

--- tg/setup/tg_install.sh
+++ tg/setup/tg_install.sh
@@ -7,13 +7,13 @@ if [ "$EUID" -ne 0 ]
 fi
 
 # Install python3
-apt-get install -y python3 python3-pip
+apt-get install -y python3 python3-pip || { echo "Failure"; exit 1; }
 
 # Install Pipenv
-pip3 install --user pipenv
+pip3 install --user pipenv || { echo "Failure"; exit 1; }
 
 # Install Python dependencies with pipenv
-python3 -m pipenv install
+python3 -m pipenv install || { echo "Failure"; exit 1; }
 
 # Install TRex
-bash ../trex/trex_installer.sh 2.92
+bash ../trex/trex_installer.sh 2.92 || { echo "Failure"; exit 1; }
